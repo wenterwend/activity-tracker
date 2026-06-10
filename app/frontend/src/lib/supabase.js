0efc156e3ce2eb1a76cf0a1f2a84dfe8bfdb5f3e
wenterwend <wenterwend@gmail.com>
2026-06-09 22:14:25 -0400

    moved the console log

--- app/frontend/src/lib/supabase.js
+++ app/frontend/src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-
+console.log('SUPABASE URL:', import.meta.env.VITE_SUPABASE_URL)
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
 )
-console.log('SUPABASE URL:', import.meta.env.VITE_SUPABASE_URL)
