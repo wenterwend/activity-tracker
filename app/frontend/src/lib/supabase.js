4a8c1cea93f1367e5acc2b4570431e128475f877
wenterwend <wenterwend@gmail.com>
2026-06-09 22:21:32 -0400

    remove logging

--- app/frontend/src/lib/supabase.js
+++ app/frontend/src/lib/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-console.log('SUPABASE URL:', import.meta.env.VITE_SUPABASE_URL)
+
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY
