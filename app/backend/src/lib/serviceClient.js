c4e5c65351e523083ffd60aceededb0d857bfd4d
wenterwend <wenterwend@gmail.com>
2026-06-16 22:42:27 -0400

    backend changes to further support admin

--- app/backend/src/lib/serviceClient.js
+++ app/backend/src/lib/serviceClient.js
@@ -3,5 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 export const serviceClient = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY,
-  { auth: { persistSession: false } }
+  {
+    auth: { persistSession: false, autoRefreshToken: false },
+    global: {
+      headers: {
+        Authorization: `Bearer ${process.env.SUPABASE_SERVICE_ROLE_KEY}`,
+      },
+    },
+  }
 )
